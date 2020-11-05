a20c325cee242fd7d422b4fc6ad7640edb24f88b
Viacheslav ZAITCEV <viacheslav.zaitcev.auditeur@lecnam.net>
2020-11-05 13:44:13 +0100

    add session dans la route /

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,19 @@ var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.session.user != undefined){  
+    res.render('index');
+  }else{
+    res.render('home', { });
+  }
 });
 
 router.get('/home', function(req, res, next) {
+  if (req.session.user != undefined){  
+    res.render('index');
+  }else{
+    registrUser(req);
+  }
   res.render('home', { });
 });
 
